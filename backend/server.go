7b31db966bb81f1169fcf9b3ef38896e1e3e68d9
Jacob Pernell <jacob.pernell@gmail.com>
2022-06-21 23:21:10 -0700

    Add test JSON with successful server response

--- backend/server.go
+++ backend/server.go
@@ -5,10 +5,22 @@ import (
 	"net/http"
 )
 
+type TestStruct struct {
+	Username string `json:"username" xml:"username"`
+	Age      int    `json:"age" xml:"age"`
+	NumPosts int    `json:"numPosts" xml:"numPosts"`
+}
+
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		testJSON := &TestStruct{
+			Username: "jacob",
+			Age:      34,
+			NumPosts: 938409,
+		}
+
+		return c.JSON(http.StatusOK, testJSON)
 	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
