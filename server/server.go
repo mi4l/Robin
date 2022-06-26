89b3a1f2ad05d9f2fb18c8d4088f7b0a9b2a39e9
Jacob Pernell <Jacob.pernell@gmail.com>
2022-06-25 22:10:08 -0700

    [37]: Create data for /content endpoint (#38)
    * [37] Create data for /content endpoint
    
    * [37]: Renamed/restructure structs
    
    * [37]: Change 'Response' to 'ContentResponse'

--- server/server.go
+++ server/server.go
@@ -1,15 +1,42 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/labstack/echo/v4"
 )
 
+type ContentResponseMeta struct {
+	Total int `json:"total"`
+}
+
+type Content struct {
+	Body string `json:"body"`
+}
+
+type ContentResponse struct {
+	Meta ContentResponseMeta `json:"meta"`
+	Data []Content `json:"data"`
+}
+
+
 func main() {
 	e := echo.New()
+
 	e.GET("/", func(c echo.Context) error {
 
 		return c.File("../templates/index.html")
 		
 	})
+
+	res := &ContentResponse{
+		Meta: ContentResponseMeta{},
+		Data: []Content{},
+	}
+
+	e.GET("/content", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, res)
+	})
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
