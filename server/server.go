b671df484ae9302a15dc7d8f01a62c42bce6f25d
Jacob Pernell <Jacob.pernell@gmail.com>
2022-07-02 19:14:16 -0700

    [30]: Enable post to FB (#40)
    * [30]: Started Go function to handle POST req
    
    * [30]: Postman can send POST req
    
    * [30]: Add POST route with Echo
    
    * [30]: Enable Echo to listen for multiple query params
    
    * [30]: Successfully receive application/json POST request from Postman
    
    * [30]: Fix route name, struct location
    
    * [30]: Minor rename of Content struct json propery
    
    * [30]: Rename propery in Content struct
    
    * [30]: Update gitignore to exclude new secrets API keys testing file
    
    * [30]: Setting up FB POST route and handler function
    
    * [30]: Successfully POST request to FB API via /content route
    
    * [30]: Rename functions/variables for legibility

--- server/server.go
+++ server/server.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -20,6 +24,33 @@ type ContentResponse struct {
 	Data []Content `json:"data"`
 }
 
+func requestPOSTFacebookContent(c echo.Context) error {
+  postURL := fmt.Sprintf("https://graph.facebook.com/%s/feed", FB_PAGE_ID)
+
+  postBody, _ := json.Marshal(map[string]string{
+    "message":  "Hello from Go!",
+    "access_token": FB_PAGE_ACCESS_TOKEN,
+  })
+  requestBody := bytes.NewBuffer(postBody)
+
+  response, err := http.Post(postURL, "application/json", requestBody)
+
+  if err != nil {
+    log.Fatalf("An Error Occured %v", err)
+  }
+  
+  defer response.Body.Close()
+
+  body, err := ioutil.ReadAll(response.Body)
+  
+  if err != nil {
+    log.Fatalln(err)
+  }
+  fmt.Printf("server: request body: %s\n", body)
+
+  return c.String(http.StatusOK, "Successfully posted to Facebook!")
+}
+
 func main() {
 	e := echo.New()
 
@@ -28,8 +59,8 @@ func main() {
 		return c.File("../templates/index.html")
 		
 	})
-
-	res := &ContentResponse{
+	
+  res := &ContentResponse{
 		Meta: ContentResponseMeta{},
 		Data: []Content{},
 	}
@@ -38,15 +69,7 @@ func main() {
 		return c.JSON(http.StatusOK, res)
 	})
 
-  e.POST("/content", func(c echo.Context) (err error) {
-    content := new(Content)
-    if err = c.Bind(content); err != nil {
-      fmt.Printf("There was an error processing the POST request. Error: %s", err.Error())
-      return err
-    }
-
-    return c.JSON(http.StatusOK, content)
-  })
+  e.POST("/content", requestPOSTFacebookContent)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
