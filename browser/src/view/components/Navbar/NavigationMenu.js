5e9a24b742518233a950cd92d3cae2c034a1c4ef
Jacob Pernell <Jacob.pernell@gmail.com>
2022-07-18 23:10:52 -0700

    [43] Set up React deps and start navbar component (#44)
    * [43] Set up React deps and start navbar component
    
    * [43] Add navbar props interface and sass
    
    * [43] Add svg icons with initial styling
    
    * [43] Fix svg attributes, sass watch, packages cleanup, generated files
    
    * [43] Remove process from package.json

---
+++ browser/src/view/components/Navbar/NavigationMenu.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './NavigationMenu.scss';
+
+export const NavigationMenu = ({ links }) => {
+  return (
+    <nav className='navbar'>
+      <ul className='nav-user-actions'>
+        {links.map(link => {
+          return (
+            <li className='nav-icon-container' key={link.name}>
+              <link.Icon />
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+NavigationMenu.propTypes = {
+  links: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      path: PropTypes.string.isRequired,
+      Icon: PropTypes.func.isRequired,
+      isExternal: PropTypes.bool.isRequired,
+    }).isRequired
+  ).isRequired,
+};
