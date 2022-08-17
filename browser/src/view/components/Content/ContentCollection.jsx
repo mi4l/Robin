24b93c6866d6cdd77d6d6e2cbec449f3b7f5b383
Jacob Pernell <Jacob.pernell@gmail.com>
2022-08-17 13:00:28 -0700

    [29]: Create initial content table (#47)
    * [29]: Create initial content table
    
    * [29] Add Skeleton component
    
    * [29] Rename variables, separate skeleton component, update styling organization
    
    * [29] Change file names

---
+++ browser/src/view/components/Content/ContentCollection.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './content-collection.scss';
+import { ContentSkeleton } from './ContentSkeleton';
+
+export const ContentCollection = () => {
+  return (
+    <table className='content-collection'>
+      <colgroup>
+        <col className='content-collection__accounts'></col>
+        <col className='content-collection__description'></col>
+        <col className='content-collection__date'></col>
+        <col className='content-collection__labels'></col>
+        <col className='content-collection__status'></col>
+      </colgroup>
+      <thead>
+        <tr>
+          <th>Accounts</th>
+          <th>Description</th>
+          <th>Date</th>
+          <th>Labels</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <ContentSkeleton />
+    </table>
+  );
+};
---
+++ browser/src/view/components/Skeleton/Skeleton.jsx
@@ -0,0 +1,6 @@
+import React from 'React';
+import './skeleton.scss';
+
+export const Skeleton = () => {
+  return <div className='skeleton'></div>;
+};
